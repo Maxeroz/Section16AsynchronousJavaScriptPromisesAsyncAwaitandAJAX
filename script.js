f20168535cfebb011e90f5bb307148473408d0b1
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-10-06 21:01:35 +0400

    Consuming Promises

--- script.js
+++ script.js
@@ -50,24 +50,24 @@ const countriesContainer = document.querySelector('.countries');
 
 // [OPTIONAL] How the Web Works: Requests and Responses
 
-// const renderCountry = function (data, className = '') {
-//   const html = `
-//   <article class="country ${className}">
-//       <img class="country__img" src="${data.flag}" />
-//       <div class="country__data">
-//           <h3 class="country__name">${data.name}</h3>
-//           <h4 class="country__region">${data.region}</h4>
-//           <p class="country__row"><span>👫</span>${(
-//             +data.population / 1000000
-//           ).toFixed(1)} people</p>
-//           <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//           <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//       </div>
-//   </article>`;
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+  </article>`;
 
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 // const getCoutryAndNeighbour = function (country) {
 //   // AJAX call country 1
@@ -130,7 +130,52 @@ const countriesContainer = document.querySelector('.countries');
 
 // Promises and the Fetch API
 
-const request = fetch(
-  `https://countries-api-836d.onrender.com/countries/name/russia`
-);
-console.log(request);
+// const request = fetch(
+//   `https://countries-api-836d.onrender.com/countries/name/russia`
+// );
+// console.log(request);
+
+// setTimeout(() => console.log(request), 3000);
+
+// Consuming Promises
+
+// const getCoutryData = function (country) {
+//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+//     .then(function (repsonse) {
+//       console.log(repsonse);
+
+//       return repsonse.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCoutryData = function (country) {
+  // Country 1
+  fetch(
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  ).then(response =>
+    response
+      .json()
+      .then(data => {
+        renderCountry(data[0]);
+
+        const neighbour = data[0].borders?.[0];
+
+        // Country 2
+
+        return fetch(
+          `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+        );
+      })
+      .then(response =>
+        response.json().then(data => renderCountry(data, 'neighbour'))
+      )
+  );
+};
+
+getCoutryData('russia');
+
+// Chaining Promises
