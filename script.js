0f4177a25ea03e0c20c49abcb17b6486fdaede2e
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-10-10 19:57:46 +0400

    The Event Loop in Practice

--- script.js
+++ script.js
@@ -352,3 +352,15 @@ whereAmI(52.508, 13.381, '715881696036124439037x17745');
 
 */
 // Asynchronous Behind the Scenes: The Event Loop
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+
+  console.log(res);
+});
+
+console.log('Test end');
